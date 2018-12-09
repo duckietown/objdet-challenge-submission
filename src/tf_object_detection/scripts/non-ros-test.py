c7f0c73dca5b45ad76d6b902e86575331cf3e0b5
Hari <g.x.zhou@outlook.com>
2018-12-09 23:25:30 +0100

    Initialize

---
+++ src/tf_object_detection/scripts/non-ros-test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import os
+import cv2
+
+sys.path.append('/Users/zhou/Downloads/tf_object_detection-0.1.0/src')
+
+import object_detection_lib
+
+# Create the instance of ObjectDetection
+odc = object_detection_lib.ObjectDetection('models/research/object_detection', 0.5)
+
+cvimg = cv2.imread("/Users/zhou/Desktop/duckietown/duckietown_raw_dataset/all_images/average/b_BR_doort_frame00212.jpg")
+
+# Detect the objects
+object_names = odc.scan_for_objects(cvimg)
+print(object_names)
+
+cv2.imshow('object detection', cvimg)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite('adjusted_test_image.jpg', cvimg)
+
+
