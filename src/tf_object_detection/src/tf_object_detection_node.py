ce3f4b869713a5f2be53bdd6b341f735aef9670f
Hari <g.x.zhou@outlook.com>
2018-12-12 20:16:13 +0100

    Chnage the name of topics and disable tf_conversions

--- src/tf_object_detection/src/tf_object_detection_node.py
+++ src/tf_object_detection/src/tf_object_detection_node.py
@@ -8,61 +8,54 @@ import object_detection_lib
 from tf_object_detection.msg import detection_results
 from std_msgs.msg import Empty
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 
 class ObjectDetectionNode:
     def __init__(self):
         self.__bridge = CvBridge()
         # Publisher to publish update image
-        self.__image_pub = rospy.Publisher("tf_object_detection_node/adjusted_image", Image, queue_size=1)
+        self.__image_pub = rospy.Publisher('/'+os.environ['DUCKIEBOT_NAME']+'/prediction_images', CompressedImage, queue_size=1)
         # Publisher to publish the result
-        self.__result_pub = rospy.Publisher("tf_object_detection_node/result", detection_results, queue_size=1)
+        self.__result_pub = rospy.Publisher('/'+os.environ['DUCKIEBOT_NAME'] +"/predicted_result", detection_results, queue_size=1)
         # Subscribe to topic which will kick off object detection in the next image
-        self.__command_sub = rospy.Subscriber("tf_object_detection_node/start", Empty, self.StartCallback)
+        # self.__command_sub = rospy.Subscriber("tf_object_detection_node/start", Empty, self.StartCallback)
         # Subscribe to the topic which will supply the image fom the camera
-        self.__image_sub = rospy.Subscriber("camera/image/raw",Image, self.Imagecallback)
+        self.__image_sub = rospy.Subscriber('/'+ os.environ['DUCKIEBOT_NAME'] + "/camera_node/image/compressed",CompressedImage, self.Imagecallback)
 
         # Flag to indicate that we have been requested to use the next image
-        self.__scan_next = False
+        # self.__scan_next = False
 
-        # Read the path for models/research/object_detection directory from the parameter server (which is configured in
-        # .yaml file) or use this default
-        rospy.set_param('/object_detection/path', os.path.abspath('../models/research'))
-        object_detection_path = rospy.get_param('/object_detection/path')
-        # object_detection_path = rospy.get_param('/object_detection/path')
-        # add models/research into PYTHONPATH such that we can use modules inside this directory
-        
         # Read the confidence level, any object with a level below this will not be used
         confidence_level = rospy.get_param('/object_detection/confidence_level', 0.50)
-
         # Create the object_detection_lib class instance
         self.__odc = object_detection_lib.ObjectDetection(confidence_level)
 
     # Callback for start command message
-    def StartCallback(self, data):
-        # Indicate to use the next image for the scan
-        self.__scan_next = True
+    # def StartCallback(self, data):
+    #     # Indicate to use the next image for the scan
+    #     self.__scan_next = True
 
     # Callback for new image received
     def Imagecallback(self, data):
-        if self.__scan_next == True:
-            self.__scan_next = False
-            # Convert the ROS image to an OpenCV image
-            image = self.__bridge.imgmsg_to_cv2(data, "bgr8")
+        # if self.__scan_next == True:
+        #     self.__scan_next = False
+        # Convert the ROS image to an OpenCV image
+        image = self.__bridge.imgmsg_to_cv2(data, "bgr8")
 
-            # The supplied image will be modified if known objects are detected
-            object_names_detected = self.__odc.scan_for_objects(image)
+        # The supplied image will be modified if known objects are detected
+        object_names_detected = self.__odc.scan_for_objects(image)
 
-            # publish the image, it may have been modified
-            try:
-                self.__image_pub.publish(self.__bridge.cv2_to_imgmsg(image, "bgr8"))
-            except CvBridgeError as e:
-                print(e)
+        # publish the image, it may have been modified
+        try:
+            self.__image_pub.publish(self.__bridge.cv2_to_imgmsg(image, "bgr8"))
+        except CvBridgeError as e:
+            print(e)
 
-            # Publish names of objects detected
-            result = detection_results()
-            result.names_detected = object_names_detected
-            self.__result_pub.publish(result)
+        # Publish names of objects detected
+        result = detection_results()
+        result.names_detected = object_names_detected
+        self.__result_pub.publish(result)
 
 def main(args):
     rospy.init_node('tf_object_detection_node', anonymous=False)
